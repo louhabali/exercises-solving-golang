a7a0ef881cb13cb31f3abaea7da132aaa1ba3d47
alouhab <alouhab@gmail.com>
2024-02-15 10:59:59 +0100

    dd

--- isnegative.go
+++ isnegative.go
@@ -5,9 +5,9 @@ import (
 )
 
 func IsNegative(nb int) {
-    if nb < 0 {
-        z01.PrintRune('T')
-    } else {
-        z01.PrintRune('F')
+	if nb < 0 {
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
     }
 }
