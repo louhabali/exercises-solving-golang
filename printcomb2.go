0fd9a4a185c844d3fad5ee855bc26f0c44ce82b9
alouhab <alouhab@gmail.com>
2024-02-15 16:47:51 +0100

    ed

--- printcomb2.go
+++ printcomb2.go
@@ -3,18 +3,23 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	end := false
-	for i := '0'; i <= '8'; i++ {
-		for j := i + 1; j <= '9'; j++ {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '8'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for f := k + 1; f <= '9'; f++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(f)
+					if j == '8' {
+						break
+					}
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 
-			if end {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+				}
 			}
-			end = true
-			z01.PrintRune(i)
-			z01.PrintRune(j)
 		}
 	}
-	z01.PrintRune('\n')
 }
