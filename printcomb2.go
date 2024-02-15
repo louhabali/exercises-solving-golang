42655449c382de3c82858f552b481bb668d338ef
alouhab <alouhab@gmail.com>
2024-02-15 17:21:17 +0100

    done

--- printcomb2.go
+++ printcomb2.go
@@ -4,23 +4,25 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j <= '8'; j++ {
+		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				for f := k + 1; f <= '9'; f++ {
-					z01.PrintRune(i)
+				for f := '0'; f <= '9'; f++ {
+					
+					if (i <= '9' && j <= '8' && k <= '9' && f <= '9' ) {
+                    z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
 					z01.PrintRune(k)
 					z01.PrintRune(f)
-					if j == '8' {
-						break
-					}
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
+					}
+				
 
 				}
 			}
 		}
 	}
 	z01.PrintRune('\n')
+
 }
