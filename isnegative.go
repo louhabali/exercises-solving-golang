c98a330a6085179bef24e763ffe802bdf25f4b0e
alouhab <alouhab@gmail.com>
2024-02-15 10:02:50 +0100

    dd

--- isnegative.go
+++ isnegative.go
@@ -1,12 +1,14 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func IsNegative(num int) {
-	if num >= 0 {
-		z01.PrintRune('F')
-	} else {
+	if num < 0 {
 		z01.PrintRune('T')
+	} else if num >= 0 {
+		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
 }
