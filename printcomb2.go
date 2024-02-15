4804eb6e01eddd90aedef49f5f4a57b68e7f28ce
alouhab <alouhab@gmail.com>
2024-02-15 17:22:30 +0100

    done

--- printcomb2.go
+++ printcomb2.go
@@ -7,22 +7,18 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for f := '0'; f <= '9'; f++ {
-					
-					if (i <= '9' && j <= '8' && k <= '9' && f <= '9' ) {
-                    z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(' ')
-					z01.PrintRune(k)
-					z01.PrintRune(f)
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+					if i <= '9' && j <= '8' && k <= '9' && f <= '9' {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(f)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 					}
-				
-
 				}
 			}
 		}
 	}
 	z01.PrintRune('\n')
-
 }
