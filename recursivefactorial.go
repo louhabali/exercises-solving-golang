7648be915ed1f4f392a38b07870de8dc272782d5
alouhab <louhabali@gmail.com>
2024-02-20 12:09:23 +0100

    factoriel done

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,12 +1,12 @@
 package piscine
 
-func recursiveFactorial(nb int) int {
+func RecursiveFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
 	if nb == 0 {
 		return 1
 	}
-	c := nb * recursiveFactorial(nb-1)
+	c := nb * RecursiveFactorial(nb-1)
 	return c
 }
