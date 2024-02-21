4d1ded16ea4069c3a409659733bc3d88e3f9270e
alouhab <louhabali@gmail.com>
2024-02-21 16:23:19 +0100

    hello

--- sqrt.go
+++ sqrt.go
@@ -3,11 +3,14 @@ package piscine
 func Sqrt(nb int) int {
 	if nb < 0 {
 		return 0
+	} else if nb == 0 || nb == 1 {
+		return nb
 	}
 	nombre := 0
 	for i := 1; i <= nb/2; i++ {
 		if i*i == nb {
 			nombre = i
+			break
 		}
 	}
 	return nombre
