68e06216858c026d3dd4a8c0b4dc782c8444b57d
alouhab <louhabali@gmail.com>
2024-02-22 10:37:01 +0100

    isalpha done

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, char := range s {
-		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= 0 && char <= 9) || (char == ' ') {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') || (char == ' ') {
 			continue
 		} else {
 			return false
