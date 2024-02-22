ba9a0f06f6df35762faf0d5d0023809c953108bb
alouhab <louhabali@gmail.com>
2024-02-22 09:44:25 +0100

    index done

---
+++ index.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	farr := []rune(toFind)
+	sarr := []rune(s)
+	for i := 0; i <= len(s)-1; i++ {
+		if sarr[i] == farr[0] {
+			return i
+		}
+	}
+	return -1
+}
