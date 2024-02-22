7b27b57dedc7d55a6a4dcc934f77807ff905e2a9
alouhab <louhabali@gmail.com>
2024-02-22 10:43:18 +0100

    isp done

---
+++ isprintable.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsPrintable(s string) bool {
+	for _, char := range s {
+		if char >= 32 && char <= 126 {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
