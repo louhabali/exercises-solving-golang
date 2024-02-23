738b17bfc1e8a8db147f4727e1199bd76a561c37
alouhab <louhabali@gmail.com>
2024-02-23 11:38:15 +0100

    program

---
+++ printparams/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		for _, str := range os.Args[i] {
+			z01.PrintRune(str)
+		}
+		z01.PrintRune('\n')
+	}
+}
