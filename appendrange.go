dbe9defff99069f3e816619a7abc376eec8cdcc3
alouhab <louhabali@gmail.com>
2024-02-23 14:11:58 +0100

    append

--- appendrange.go
+++ appendrange.go
@@ -5,7 +5,7 @@ func AppendRange(min, max int) []int {
 	if min >= max {
 		return nil
 	}
-	for i := min; i <= max; i++ {
+	for i := min; i < max; i++ {
 		table = append(table, i)
 	}
 	return table
