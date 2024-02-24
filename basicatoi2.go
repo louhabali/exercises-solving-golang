6bd67f7e37944e34491b0bcda9b06c041953f040
alouhab <louhabali@gmail.com>
2024-02-24 11:36:55 +0100

    basic

--- basicatoi2.go
+++ basicatoi2.go
@@ -4,7 +4,7 @@ func BasicAtoi2(s string) int {
 	arr := []rune(s)
 	num := 0
 	for i := 0; i < len(arr); i++ {
-		if !((arr[i] >= '0' && arr[i] <= '9') || (arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z')) {
+		if !(arr[i] >= '0' && arr[i] <= '9') {
 			return 0
 		}
 		if arr[i] >= '0' && arr[i] <= '9' {
