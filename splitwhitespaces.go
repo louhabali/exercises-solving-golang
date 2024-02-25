d6f3d511bcd8ca28ca32d4057d230d79b6494f2f
alouhab <louhabali@gmail.com>
2024-02-25 13:58:27 +0100

    done

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -7,10 +7,12 @@ func SplitWhiteSpaces(s string) []string {
 	for i := 0; i < len(sarr); i++ {
 		if sarr[i] != ' ' {
 			st += string(sarr[i])
-		} else if st != "" && st != " " && i < len(sarr) && sarr[i] == ' ' {
-			t = append(t, st)
+		} else {
+			if st != "" && st != " " && i < len(sarr) && sarr[i] == ' ' {
+				t = append(t, st)
+			}
+			st = ""
 		}
-		st = ""
 	}
 	if st == "" {
 		return nil
