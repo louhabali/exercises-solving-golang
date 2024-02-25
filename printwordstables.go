4a9ae2c49383b4558c7fd53e8dd3f11db09b7d80
alouhab <louhabali@gmail.com>
2024-02-25 14:27:31 +0100

    done

---
+++ printwordstables.go
@@ -0,0 +1,12 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintWordsTables(a []string) {
+	for _, ch := range a {
+		s := ch
+		for _, f := range s {
+			z01.PrintRune(f)
+		}
+	}
+}
