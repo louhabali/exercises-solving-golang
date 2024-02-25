e081475980a946222eb7ab89a83a9c55e3906b41
alouhab <louhabali@gmail.com>
2024-02-25 13:35:11 +0100

    done

---
+++ splitwhitespaces.go
@@ -0,0 +1,19 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var t []string
+	sarr := []rune(s)
+	st := ""
+	for i := 0; i < len(sarr); i++ {
+		if sarr[i] != ' ' {
+			st += string(sarr[i])
+		} else if st != "" && st != " " && i < len(sarr) && sarr[i] == ' ' {
+			t = append(t, st)
+		}
+		st = ""
+	}
+	if st == "" {
+		return nil
+	}
+	return t
+}
