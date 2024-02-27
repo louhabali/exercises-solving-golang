1c649f57a99b1e827e4cb040da3715c73e701349
alouhab <louhabali@gmail.com>
2024-02-27 15:55:03 +0100

    done

--- point/main.go
+++ point/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"piscine"
-
 	"github.com/01-edu/z01"
 )
 
@@ -16,19 +14,36 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func PrintNbr(nb int) {
+	n := '0'
+	subn := '0'
+	for i := 0; i < nb/10; i++ {
+		n++
+	}
+	z01.PrintRune(n)
+	for i := 0; i < nb%10; i++ {
+		subn++
+	}
+	z01.PrintRune(subn)
+}
+
 func main() {
 	points := &point{}
 
 	setPoint(points)
-	prx := "x = "
+	var p []string 
+	prx := "x = " 
 	pry := ", y = "
-	for _, char := range prx {
-		z01.PrintRune(char)
-	}
-	piscine.PrintNbr(points.x)
-	for _, char := range pry {
-		z01.PrintRune(char)
-	}
-	piscine.PrintNbr(points.y)
+	p = append(p, prx, pry)
+	for i, char := range p {
+		for _, v := range char {
+			z01.PrintRune(v)
+		}
+		if i == 0 {
+			PrintNbr(points.x)
+		} else {
+			PrintNbr(points.y)
+		}
+	}	
 	z01.PrintRune('\n')
 }
