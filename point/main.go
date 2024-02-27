e713cd2b8d63cd0ad69fdd6fbe8873da983e6700
alouhab <louhabali@gmail.com>
2024-02-27 15:16:54 +0100

    done

--- point/main.go
+++ point/main.go
@@ -13,14 +13,32 @@ func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
 }
+func PrintNbr(nb int){
+	n := '0'
+	subn := '0'
+	for i:=0; i<nb/10;i++ {
+		n++
+	}
+	z01.PrintRune(n)
+	for i:=0; i<nb%10;i++ {
+		subn ++
+	}
+	z01.PrintRune(subn)
+}
 
 func main() {
 	points := &point{}
 
 	setPoint(points)
+	z01.PrintRune('x')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	PrintNbr(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune('=')
+	PrintNbr(points.y)
 
-	str := "x = " + string('0'+(points.x/10)) + string('0'+(points.x%10)) + ", y = " + string('0'+(points.y/10)) + string('0'+(points.y%10)) + "\n"
-	for _, character := range str {
-		z01.PrintRune(character)
-	}
+
 }
