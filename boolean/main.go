9a7604d222d1751c229b2d2976640d3dcd0f9faa
alouhab <louhabali@gmail.com>
2024-02-27 12:15:29 +0100

    done

---
+++ boolean/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	
+	if nbr %2 == 0 {
+		return true
+	}
+	return false
+	
+}
+
+func main() {
+	myargs :=os.Args[1:]
+	lengthOfArg := len(myargs)
+	if isEven(lengthOfArg)==false {
+		printStr("I have an odd number of arguments")
+	} else {
+		printStr("I have an even number of arguments")
+	}
+}
