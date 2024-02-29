5126b682e073e4cf7706dcd9511a5a422f0f9ef9
alouhab <louhabali@gmail.com>
2024-02-29 22:13:40 +0100

    done

--- enigma.go
+++ enigma.go
@@ -2,9 +2,8 @@ package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
 	x := ***a
-	*******c = x
-	y := *******c
-	****d = y
-	*b = ****d
-	***a = *b
+	***a = *******c
+	*******c = ****d
+	****d = *b
+	*b = x
 }
