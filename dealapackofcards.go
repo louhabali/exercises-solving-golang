a25b85ef770e8972c20674e8d6f88f71eabb2a38
alouhab <louhabali@gmail.com>
2024-02-29 18:35:32 +0100

    done

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,14 +1,14 @@
-package main
+package piscine
 
 import "fmt"
 
-func DealAPackOfCards (deck []int) {
-	deckdividedby4 := len(deck)/4
+func DealAPackOfCards(deck []int) {
+	deckdividedby4 := len(deck) / 4
 
-	for i:= 0; i < 4; i++{
+	for i := 0; i < 4; i++ {
 		fmt.Printf("player %d: ", i+1)
-		for j:=0; j < deckdividedby4;j++ {
-			fmt.Printf("%d", deck[i*deckdividedby4 + j])
+		for j := 0; j < deckdividedby4; j++ {
+			fmt.Printf("%d", deck[i*deckdividedby4+j])
 			if j < deckdividedby4-1 {
 				fmt.Print(", ")
 			}
@@ -16,13 +16,3 @@ func DealAPackOfCards (deck []int) {
 		fmt.Println()
 	}
 }
-func main() {
-	deck := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
-	DealAPackOfCards(deck)
-}
-// $ go run . | cat -e
-// Player 1: 1, 2, 3$
-// Player 2: 4, 5, 6$
-// Player 3: 7, 8, 9$
-// Player 4: 10, 11, 12$
-// $
