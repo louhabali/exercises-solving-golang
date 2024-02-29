14a6cde3bdecc6486c4370bd8f7f96d14d530b43
alouhab <louhabali@gmail.com>
2024-02-29 22:08:11 +0100

    done

---
+++ enigma.go
@@ -0,0 +1,51 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	x := ***a
+	*******c = x
+	y := *******c
+	****d = y
+	z := ****d
+	*b = z
+	w := *b
+	***a = w
+}
+
+// func main() {
+// 	x := 5
+// 	y := &x
+// 	z := &y
+// 	a := &z
+
+// 	w := 2
+// 	b := &w
+
+// 	u := 7
+// 	e := &u
+// 	f := &e
+// 	g := &f
+// 	h := &g
+// 	i := &h
+// 	j := &i
+// 	c := &j
+
+// 	k := 6
+// 	l := &k
+// 	m := &l
+// 	n := &m
+// 	d := &n
+
+// 	fmt.Println(***a)
+// 	fmt.Println(*b)
+// 	fmt.Println(*******c)
+// 	fmt.Println(****d)
+
+// 	piscine.Enigma(a, b, c, d)
+
+// 	fmt.Println("After using Enigma")
+// 	fmt.Println(***a)
+// 	fmt.Println(*b)
+// 	fmt.Println(*******c)
+// 	fmt.Println(****d)
+
+// }
