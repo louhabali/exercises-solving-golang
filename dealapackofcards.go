ce5891e4853f8f68c47aa1eae7e0a825c1bd3ea0
alouhab <louhabali@gmail.com>
2024-02-29 18:45:50 +0100

    done

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -7,13 +7,14 @@ import (
 )
 
 func DealAPackOfCards(deck []int) {
+	deckdividedby4 := 3
+
 	for i := 0; i < 4; i++ {
 		fmt.Printf("player %d: ", i+1)
-		for j := 0; j < 3; j++ {
-			if j < 2 {
-				fmt.Printf("%d, ", deck[i*3+j])
-			} else {
-				fmt.Printf("%d", deck[i*3+j])
+		for j := 0; j < deckdividedby4; j++ {
+			fmt.Printf("%d", deck[i*deckdividedby4+j])
+			if j < deckdividedby4-1 {
+				fmt.Print(", ")
 			}
 		}
 		z01.PrintRune('\n')
