e7cb8c20bf29ba9fccf8aab8e3727b18bc7e3210
alouhab <louhabali@gmail.com>
2024-02-29 15:27:58 +0100

    done

--- abort.go
+++ abort.go
@@ -5,9 +5,7 @@ func Abort(a, b, c, d, e int) int {
 	for i := 0; i < len(arr); i++ {
 		for j := 1; j < len(arr); j++ {
 			if arr[i] > arr[j] {
-				c := arr[i]
-				arr[i] = arr[j]
-				arr[j] = c
+				arr[i], arr[j] = arr[j], arr[i]
 			}
 		}
 	}
