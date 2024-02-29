38f45a4bde90a557ee23ef08a7db0e14e1f822ed
alouhab <louhabali@gmail.com>
2024-02-29 17:23:25 +0100

    done

--- compact.go
+++ compact.go
@@ -4,7 +4,7 @@ func Compact(ptr *[]string) []string {
 	p := *ptr
 	newarr := make([]string, 0)
 	for i := 0; i < len(p); i++ {
-		if p[i] != "" {
+		if string(p[i]) != "" {
 			newarr = append(newarr, p[i])
 		}
 		*ptr = newarr
