7abaf9293fc79e43d4ddf8114ec4118f4895bc0d
alouhab <louhabali@gmail.com>
2024-03-01 01:39:16 +0100

    done

---
+++ comcheck/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	osarr := os.Args[1:]
+	for _, ch := range osarr {
+		if ch == "01" || ch == "galaxy" || ch == "galaxy 01" {
+			fmt.Println("Alert !!!")
+		} else {
+			fmt.Println("")
+		}
+	}
+}
