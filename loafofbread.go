59bda5e26eea97f7bbab2f31eb5e7a00e7d0c0c5
alouhab <louhabali@gmail.com>
2024-03-01 09:20:24 +0100

    done

---
+++ loafofbread.go
@@ -0,0 +1,21 @@
+package piscine
+
+func LoafOfBread(str string) string {
+	if len(str) < 5 {
+		return "Invalid Output\n"
+	}
+	var result string
+	count := 0
+	for i := 0; i < len(str); i++ {
+		if str[i] != ' ' {
+			result += string(str[i])
+			count++
+			if count == 5 {
+				i++
+				count = 0
+				result += " "
+			}
+		}
+	}
+	return result + "\n"
+}
