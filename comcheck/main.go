b0e1732f2062d142e6c3960780db082236da9a7f
alouhab <louhabali@gmail.com>
2024-03-01 01:43:03 +0100

    done

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,7 +9,7 @@ func main() {
 	osarr := os.Args[1:]
 	for _, ch := range osarr {
 		if ch == "01" || ch == "galaxy" || ch == "galaxy 01" {
-			fmt.Println("Alert !!!")
+			fmt.Println("Alert!!!")
 		} else {
 			fmt.Print("")
 		}
