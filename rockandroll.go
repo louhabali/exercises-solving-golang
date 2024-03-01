06df762556a18351cf2365572e6043731c6ff178
alouhab <louhabali@gmail.com>
2024-03-01 08:11:32 +0100

    done

--- rockandroll.go
+++ rockandroll.go
@@ -2,14 +2,14 @@ package piscine
 
 func RockAndRoll(n int) string {
 	if n < 0 {
-		return "error: number is negative"
+		return "error: number is negative\n"
 	} else if n%2 == 0 && n%3 == 0 {
-		return "rock and roll"
+		return "rock and roll\n"
 	} else if n%2 == 0 {
-		return "rock"
+		return "rock\n"
 	} else if n%3 == 0 {
-		return "roll"
+		return "roll\n"
 	} else {
-		return "error: non divisible"
+		return "error: non divisible\n"
 	}
 }
