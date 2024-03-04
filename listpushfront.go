54f89b89ad62cf1bc8569ef2a0f533034267e69a
alouhab <louhabali@gmail.com>
2024-03-04 18:21:43 +0100

    done

--- listpushfront.go
+++ listpushfront.go
@@ -1,22 +1,12 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListPushFront(l *List, data interface{}) {
 	nouvelle := &NodeL{Data: data}
 	if l.Head == nil {
 		l.Head = nouvelle
 		l.Tail = nouvelle
 	} else {
-		l.Tail.Next = nouvelle
-		l.Tail = nouvelle
+		nouvelle.Next = l.Head
+		l.Head = nouvelle
 	}
 }
