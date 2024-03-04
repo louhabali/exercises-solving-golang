6bad31fa33a03531e0fbcad23c5b5af03c4eadc4
alouhab <louhabali@gmail.com>
2024-03-04 17:59:08 +0100

    done

---
+++ listpushback.go
@@ -0,0 +1,22 @@
+package piscine
+
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
+
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
+
+func ListPushBack(l *List, data interface{}) {
+	nouvelle := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = nouvelle
+		l.Tail = nouvelle
+	} else {
+		l.Tail.Next = nouvelle
+		l.Tail = nouvelle
+	}
+}
