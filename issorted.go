7c955c04939727f3d140963eaecb6efcd4ce535a
alouhab <louhabali@gmail.com>
2024-03-05 12:24:52 +0100

    done

--- issorted.go
+++ issorted.go
@@ -1,7 +1,7 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	if len(a) > 0 {
+	if len(a) > 1 {
 		if f(a[0], a[1]) <= 0 {
 			for i := 0; i < len(a)-1; i++ {
 				if f(a[i], a[i+1]) > 0 {
