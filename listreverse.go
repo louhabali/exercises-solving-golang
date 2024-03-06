ffcf44d414b7fdd7b28d85204523c1993a48926c
alouhab <louhabali@gmail.com>
2024-03-06 10:35:04 +0100

    done

--- listreverse.go
+++ listreverse.go
@@ -1,59 +1,14 @@
-package main
+package piscine
 
-import (
-	"fmt"
-)
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
-func ListPushBack(l *List, data interface{}) {
-	nouvelle := &NodeL{Data: data}
-	if l.Head == nil {
-		l.Head = nouvelle
-		l.Tail = nouvelle
-	} else {
-		l.Tail.Next = nouvelle
-		l.Tail = nouvelle
-	}
-}
 func ListReverse(l *List) {
 	var previous *NodeL
-	actuel := l.Head
+	c := l.Head
 	l.Tail = l.Head
-	for actuel != nil {
-		next := actuel.Next
-		actuel.Next = previous
-		previous = actuel
-		actuel = next
+	for c != nil {
+		nx := c.Next
+		c.Next = previous
+		previous = c
+		c = nx
 	}
 	l.Head = previous
-}
-
-func main() {
-	link := &List{}
-
-	ListPushBack(link, 1)
-	ListPushBack(link, 2)
-	ListPushBack(link, 3)
-	ListPushBack(link, 4)
-
-	ListReverse(link)
-
-	it := link.Head
-
-	for it != nil {
-		fmt.Println(it.Data)
-		it = it.Next
-	}
-
-	fmt.Println("Tail", link.Tail)
-	fmt.Println("Head", link.Head)
 }
