0803b0336d36be63a62d20607fd9e8e00388cefc
alouhab <louhabali@gmail.com>
2024-03-07 11:01:44 +0100

    done

--- listfind.go
+++ listfind.go
@@ -3,6 +3,7 @@ package piscine
 func CompStr(a, b interface{}) bool {
 	return a == b
 }
+
 func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
 	init := l.Head
 	for init != nil {
