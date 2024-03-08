2f95c449009aa0f12c57fe7b0419f084dd29851f
alouhab <louhabali@gmail.com>
2024-03-08 09:46:08 +0100

    done search

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,15 +1,20 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	return searchItemHelper(root, elem, nil)
+}
+
+func searchItemHelper(root *TreeNode, elem string, parent *TreeNode) *TreeNode {
 	if root == nil || root.Data == elem {
+		if root != nil {
+			root.Parent = parent
+		}
 		return root
 	}
 
 	if elem < root.Data {
-		found := BTreeSearchItem(root.Left, elem)
-		if found != nil {
-			return found
-		}
+		return searchItemHelper(root.Left, elem, root)
 	}
-	return BTreeSearchItem(root.Right, elem)
+
+	return searchItemHelper(root.Right, elem, root)
 }
